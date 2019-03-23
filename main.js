7812d06c640ae7a5b50df0c42c7d40194cd74a37
Dneal620 <45676489+Dneal620@users.noreply.github.com>
2019-03-23 13:52:57 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+let sliderImages = document.querySelectorAll('.slide');
+let arrowLeft = document.querySelector('#arrow-left');
+let arrowRight = document.querySelector('#arrow-right');
+let current = 0;
+
+
+// Clear All Images
+function reset(){
+	for(let i = 0; i < sliderImages.length; i++){
+		sliderImages[i].style.display = 'none';
+	}
+}
+
+
+// Init Slider
+function startSlide(){
+	reset();
+	sliderImages[0].style.display = 'block';
+}
+
+
+// Show Previous
+function slideLeft(){
+		reset();
+		sliderImages[current - 1].style.display = 'block';
+		current--;
+}
+
+//show Next
+function slideRight(){
+		reset();
+		sliderImages[current + 1].style.display = 'block';
+		current++;
+}
+
+// Left Arrow Click
+arrowLeft.addEventListener('click', function(){
+	if(current === 0){
+		current = sliderImages.length
+	}
+	slideLeft();
+});
+
+//Right Arrow Click
+arrowRight.addEventListener('click', function(){
+	if(current === sliderImages.length -1){
+		current = -1
+	}
+	slideRight();
+});
+
+
+startSlide();
