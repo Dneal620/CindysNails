f8e15ab05f1ecfd198957a11ed4cd5191de6fa39
Dan Neal <dneal620@gmail.com>
2019-03-30 10:42:21 -0700

    consolidated nav.js into main.js to fix issue i was having

--- js/main.js
+++ js/main.js
@@ -1,3 +1,53 @@
+//nav programing
+let nav = document.querySelectorAll('.nav');
+
+let nav1 = document.querySelector('.nav1');
+let nav2 = document.querySelector('.nav2');
+let nav3 = document.querySelector('.nav3');
+let nav4 = document.querySelector('.nav4');
+let nav5 = document.querySelector('.nav5');
+let nav6 = document.querySelector('.nav6');
+
+
+function resetNav(){
+	nav.forEach(function(a){
+    a.style.color = "black";
+  });
+}
+
+
+nav1.addEventListener('click', function(){
+  resetNav();
+  nav1.style.color = 'pink';
+})
+
+nav2.addEventListener('click', function(){
+  resetNav();
+  nav2.style.color = 'pink';
+})
+
+nav3.addEventListener('click', function(){
+  resetNav();
+  nav3.style.color = 'pink';
+})
+
+nav4.addEventListener('click', function(){
+  resetNav();
+  nav4.style.color = 'pink';
+})
+
+nav5.addEventListener('click', function(){
+  resetNav();
+  nav5.style.color = 'pink';
+})
+
+nav6.addEventListener('click', function(){
+  resetNav();
+  nav6.style.color = 'pink';
+})
+
+
+//slider programing
 let sliderImages = document.querySelectorAll('.slide');
 let arrowLeft = document.querySelector('#arrow-left');
 let arrowRight = document.querySelector('#arrow-right');
