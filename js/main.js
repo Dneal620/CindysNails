36d1b0ac00821afc13688e026064fb8575fbe130
Dan Neal <dneal620@gmail.com>
2019-03-31 13:56:57 -0700

    Fixed javascript issues and formated web page

--- js/main.js
+++ js/main.js
@@ -1,73 +1,3 @@
-//Scroll Event Listener
-function Scroll(){
-	let ypos = window.pageYOffset;
-	console.log(ypos);
-}
-
-window.addEventListener('scroll', Scroll);
-
-//Slider Arrow Animation
-
-function showArrows(){
-	let leftArrow = document.querySelector('#arrow-left');
-	let rightArrow = document.querySelector('#arrow-right');
-
-	leftArrow.style.opacity = "1";
-	leftArrow.style.transform = "translateX(0px)"
-	rightArrow.style.opacity = "1";
-	rightArrow.style.transform = "translateX(0px)"
-}
-
-window.addEventListener('load', showArrows());
-//nav programing
-let nav = document.querySelectorAll('.nav');
-
-let nav1 = document.querySelector('.nav1');
-let nav2 = document.querySelector('.nav2');
-let nav3 = document.querySelector('.nav3');
-let nav4 = document.querySelector('.nav4');
-let nav5 = document.querySelector('.nav5');
-let nav6 = document.querySelector('.nav6');
-
-
-function resetNav(){
-	nav.forEach(function(a){
-    a.style.color = "black";
-  });
-}
-
-
-nav1.addEventListener('click', function(){
-  resetNav();
-  nav1.style.color = 'pink';
-})
-
-nav2.addEventListener('click', function(){
-  resetNav();
-  nav2.style.color = 'pink';
-})
-
-nav3.addEventListener('click', function(){
-  resetNav();
-  nav3.style.color = 'pink';
-})
-
-nav4.addEventListener('click', function(){
-  resetNav();
-  nav4.style.color = 'pink';
-})
-
-nav5.addEventListener('click', function(){
-  resetNav();
-  nav5.style.color = 'pink';
-})
-
-nav6.addEventListener('click', function(){
-  resetNav();
-  nav6.style.color = 'pink';
-})
-
-
 //slider programing
 let sliderImages = document.querySelectorAll('.slide');
 let arrowLeft = document.querySelector('#arrow-left');
@@ -122,3 +52,88 @@ arrowRight.addEventListener('click', function(){
 
 
 startSlide();
+
+//Scroll Event Listener
+function Scroll(){
+	let ypos = window.pageYOffset;
+	let pic1 = document.querySelector('.s2d1');
+	let pic2 = document.querySelector('.s2d2');
+	let pic3 = document.querySelector('.s2d3');
+
+
+	if(ypos >= 890){
+		pic1.style.opacity = "1";
+		pic1.style.transform = "translateX(0px)";
+
+		pic2.style.opacity = "1";
+		pic2.style.transform = "translateX(0px)";
+
+		pic3.style.opacity = "1";
+		pic3.style.transform = "translateX(0px)";
+	}
+}
+
+window.addEventListener('scroll', Scroll);
+
+//Slider Arrow Animation
+function showArrows(){
+	let leftArrow = document.querySelector('#arrow-left');
+	let rightArrow = document.querySelector('#arrow-right');
+
+	leftArrow.style.opacity = "1";
+	leftArrow.style.transform = "translateX(0px)"
+	rightArrow.style.opacity = "1";
+	rightArrow.style.transform = "translateX(0px)"
+}
+
+window.addEventListener('load', showArrows());
+
+//nav programing
+let nav = document.querySelectorAll('.nav');
+
+let nav1 = document.querySelector('.nav1');
+let nav2 = document.querySelector('.nav2');
+let nav3 = document.querySelector('.nav3');
+let nav4 = document.querySelector('.nav4');
+let nav5 = document.querySelector('.nav5');
+let nav6 = document.querySelector('.nav6');
+
+
+function resetNav(){
+	nav.forEach(function(a){
+    a.style.color = "black";
+  });
+}
+
+
+nav1.addEventListener('click', function(){
+  resetNav();
+  nav1.style.color = 'pink';
+})
+
+nav2.addEventListener('click', function(){
+  resetNav();
+  nav2.style.color = 'pink';
+})
+
+nav3.addEventListener('click', function(){
+  resetNav();
+  nav3.style.color = 'pink';
+})
+
+nav4.addEventListener('click', function(){
+  resetNav();
+  nav4.style.color = 'pink';
+})
+
+nav5.addEventListener('click', function(){
+  resetNav();
+  nav5.style.color = 'pink';
+})
+
+nav6.addEventListener('click', function(){
+  resetNav();
+  nav6.style.color = 'pink';
+})
+
+
